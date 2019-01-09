7b6d5b30df81d04227553e8e555cd2f7fcd2d0d5
Kaoru <kaoru.a992@gmail.com>
2019-01-09 18:09:16 +0900

    追加課題6:引き分けの時はDrawを表示

--- src/index.js
+++ src/index.js
@@ -144,6 +144,8 @@ class Game extends React.Component {
     let status;
     if (winInfo.winner) {
       status = "Winner: " + winInfo.winner;
+    } else if(!current.squares.includes(null)) {
+      status = "Draw";
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
